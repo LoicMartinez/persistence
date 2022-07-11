9b643efeb648579c9a83e4a137f46b3045d2e2c9
Loïc <loicmartinez3000@gmail.com>
2022-07-11 14:01:22 +0200

    add dotenv

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,6 +75,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -82,7 +83,15 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
-        'PORT': '3306'
+        'PORT': '3306',
+        # if no ssl is use, delete those lines
+        'OPTIONS':  {
+            'ssl': {
+                'ca': os.getenv("ca-cert"),
+                'cert': os.getenv("client-client"),
+                'key': os.getenv("client-key")
+            }
+        }
     }
 }
 
